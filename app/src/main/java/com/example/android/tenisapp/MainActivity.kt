7e509a97e3287c130491856feb2a119f29757e28
Bruno Paiva <brunofpaiva@gmail.com>
2018-04-13 17:12:00 -0300

    Player Profile Activity Linked.

--- app/src/main/java/com/example/android/tenisapp/MainActivity.kt
+++ app/src/main/java/com/example/android/tenisapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.android.tenisapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linkToPlayerProfile.onClick {
+            startActivity<PlayerProfileActivity>()
+        }
     }
 }
